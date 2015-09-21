1f6b704f82d6be28c72357529bfc9d21b14bda15
Stefan Teixeira <stefanfk@gmail.com>
2015-09-21 02:11:06 -0300

    Minor adjusts in test script

--- test/test.py
+++ test/test.py
@@ -6,15 +6,19 @@ driver = webdriver.Firefox()
 driver.implicitly_wait(10)
 driver.get("http://localhost:5000")
 if not "TodoApp" in driver.title:
-    raise Exception("Unable to load google page!")
+    raise Exception("Unable to load page!")
+
 btnNewTodo = driver.find_element_by_id("new")
 btnNewTodo.click()
+
 title = driver.find_element_by_id("title")
 text = driver.find_element_by_id("text")
 btnCreate = driver.find_element_by_id("save")
+
 title.send_keys("New TODO")
 text.send_keys("Describe the task clearly")
 btnCreate.click()
+
 assert driver.current_url == "http://localhost:5000/"
 
 driver.quit()
